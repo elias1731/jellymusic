b97829ff9c52f8cca2e3addc0f708211e5bcff40
𝓔𝓵𝓲𝓪𝓼 <99972450+elias1731@users.noreply.github.com>
2026-06-29 23:00:28 +0200

    deps
    @vitejs/plugin-legacy@7 core-js regenerator-runtime

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
@@ -6,26 +7,44 @@ const version = process.env.npm_package_version || 'unknown'
 
 export default defineConfig({
     base: process.env.URL_BASE_PATH || '/',
+
     plugins: [
         react(),
+
+        legacy({
+            targets: ['Edge >= 18'],
+            modernPolyfills: true,
+            renderLegacyChunks: true,
+        }),
+
         VitePWA({
             registerType: 'autoUpdate',
+
             workbox: {
                 globPatterns: ['**/*.{js,css,html,ico,png,webp,svg}'],
                 cleanupOutdatedCaches: true,
                 skipWaiting: true,
                 clientsClaim: true,
             },
+
             manifest: {
-                name: 'Jelly Music App',
-                short_name: 'JMA',
-                description: 'A lightweight & elegant music interface for Jellyfin',
-                start_url: '/',
+                name: '𝓙𝓮𝓵𝓵𝔂•Music🗿',
+                short_name: 'JellyMusic🗿',
+                description: '𝓙𝓮𝓵𝓵𝔂𝓯𝓲𝓷',
+
+                lang: 'de',
+
+                id: 'https://heyfordy.dev/radio',
+                start_url: 'https://heyfordy.dev/radio/',
+
                 scope: '/',
+
                 display: 'standalone',
                 orientation: 'portrait',
-                theme_color: '#f8f8f8',
+
                 background_color: '#f8f8f8',
+                theme_color: '#f8f8f8',
+
                 icons: [
                     {
                         src: './web-app-manifest-192x192.png',
@@ -66,13 +85,20 @@ export default defineConfig({
                 ],
             },
         }),
+
         {
             name: 'html-version-injector',
+
             transformIndexHtml(html) {
                 return html.replace('__VERSION__', version)
             },
         },
     ],
+
+    build: {
+        target: 'es2017',
+    },
+
     define: {
         __NPM_LIFECYCLE_EVENT__: JSON.stringify(process.env.npm_lifecycle_event),
         __VERSION__: JSON.stringify(version),
